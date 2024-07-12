3895e63d163d7a52132b388a2ec31e959e058e5e
CrocAI <monytiktok001@gmail.com>
2024-07-12 16:20:24 -0400

    up to beta.2

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@
 // Utilities
 
 // The Webinix core version to download
-const WebinixCoreVersion = '2.4.2';
+const WebinixCoreVersion = '2.5.0-beta.2';
 
 // Combine paths
 function joinPath(...segments: string[]): string {
