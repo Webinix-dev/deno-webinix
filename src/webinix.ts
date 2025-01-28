41b2fe0a84c1dd5e03a618fe3132606b81a152b1
CrocAI <monytiktok001@gmail.com>
2025-01-28 13:02:03 -0500

    Update date

--- src/webinix.ts
+++ src/webinix.ts
@@ -2,7 +2,7 @@
   Webinix Deno 2.5.2
   http://webinix.me
   https://github.com/webinix-dev/deno-webinix
-  Copyright (c) 2020-2023 Hassan Draga.
+  Copyright (c) 2020-2025 Hassan Draga.
   Licensed under MIT License.
   All rights reserved.
   Canada.
